eabeace8ea4f8d800806ee586930251017ab9633
achmadss <anakinskywalk1@gmail.com>
2023-05-15 21:20:46 +0700

    feat: source module

--- src/source/source.module.ts
+++ src/source/source.module.ts
@@ -3,9 +3,11 @@ import { AsurascansController } from './asurascans/asurascans.controller';
 import { AsurascansService } from './asurascans/asurascans.service';
 import { SourceController } from './source.controller';
 import { SourceService } from './source.service';
+import { MangaparkController } from './mangapark/mangapark.controller';
+import { MangaparkService } from './mangapark/mangapark.service';
 
 @Module({
-    controllers: [AsurascansController, SourceController],
-    providers: [AsurascansService, SourceService]
+    controllers: [AsurascansController, SourceController, MangaparkController],
+    providers: [AsurascansService, SourceService, MangaparkService]
 })
 export class SourceModule { }
